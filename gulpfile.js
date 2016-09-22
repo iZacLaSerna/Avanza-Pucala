d75b3638c9ae88c13f35838745a0a0a2f50269a6
Isaac La Serna Castillo <iclasac@gmail.com>
2016-09-22 05:00:05 -0500

    Utilizando post css para dar estilo al sitio

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var rucksack = require('rucksack-css');
+var cssnext = require('postcss-cssnext');
+var cssnested = require('postcss-nested');
+var mixins = require('postcss-mixins');
+var atImport = require('postcss-import');
+var csswring = require('csswring');
+var mqpacker = require('css-mqpacker');
+var browserSync = require('browser-sync').create();
+
+
+//Servidor de desarrollo
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+})
+//Tarea para procesar el css
+gulp.task('css', function(){
+  var processors = [
+    atImport(),
+    mixins(),
+    cssnested,
+    rucksack(),
+    cssnext({browsers:['> 5%', 'ie 8']}),
+    mqpacker(),
+    // csswring()
+  ]
+  return gulp.src('./src/estilos.css')
+  .pipe(postcss(processors))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.stream())
+})
+
+// Tarea para vigilar los cambios
+gulp.task('watch', function(){
+  gulp.watch('./src/*.css', ['css'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['watch', 'serve'])
